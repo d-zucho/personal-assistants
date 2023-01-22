0419b03855709dca5c4ad77d24f851189c8d3b58
D_Zucho <danny.nico.dev@outlook.com>
2023-01-21 23:38:42 -0800

    add bulma styling

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,49 @@ import AlexaImg from './assets/alexa.png'
 import SiriImg from './assets/siri.png'
 import CortanaImg from './assets/cortana.png'
 
+// * Styles
+import 'bulma/css/bulma.css'
+
 function App() {
   return (
-    <div className='App'>
-      <h1>from app</h1>
-      <img src={AlexaImg} alt='Something' />
-      <ProfileCard title={'Alexa'} handle='@alexa99' />
-      <img src={SiriImg} alt='Siri' />
-      <ProfileCard title={'Siri'} handle='@siri01' />
-      <img src={CortanaImg} alt='Cortana' />
-      <ProfileCard title={'Alexa'} handle='@cortana32' />
+    <div>
+      <section class='hero is-primary'>
+        <div class='hero-body'>
+          <p class='title'>Personal Digital Assistants</p>
+        </div>
+      </section>
+
+      <div className='container'>
+        <section className='section'>
+          <h1>from app</h1>
+          <div className='columns'>
+            <div className='column is-3 is-offset-1'>
+              <ProfileCard
+                title={'Alexa'}
+                handle='@alexa99'
+                image={AlexaImg}
+                description='Alexa was created by Amazon and helps.'
+              />
+            </div>
+            <div className='column is-3'>
+              <ProfileCard
+                title={'Siri'}
+                handle='@siri01'
+                image={SiriImg}
+                description='Siri is made by Apple. It is being phased out.'
+              />
+            </div>
+            <div className='column is-3'>
+              <ProfileCard
+                title={'Cortana'}
+                handle='@cortana32'
+                image={CortanaImg}
+                description='Cortana was made by Microsoft. Who knows?'
+              />
+            </div>
+          </div>
+        </section>
+      </div>
     </div>
   )
 }
